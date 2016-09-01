7348eb2cb82dfe4bd69e6fbca4d82ae47cf980e1
fausto bischoff <fausto-bischoff@detran.rs.gov.br>
2016-09-01 18:01:50 -0300

    Primeiro commit

---
+++ js/main.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $(".abelha").click(function(){
+        $('.conteinerLargaLetra').css("background-color", "red");
+    });
+     
+    $( ".arrastar" ).draggable();
+    
+  
+});
